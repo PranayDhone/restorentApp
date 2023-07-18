e83fc3be92f6bfe3ef407325ee116f54fa5de9ef
PranayDhone <pranayadhone18@gmail.com>
2023-07-18 13:40:00 +0530

    use Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/Cart-context";
 import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
+  const cartctx = useContext(CartContext);
+  const numberOfCartItems = cartctx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
   return (
     //NOTE-=> The <span> tag is much like the <div> element, but <div> is a block-level element and <span> is an inline element.
     <button className={classes.button} onClick={props.onClick}>
@@ -8,7 +14,7 @@ const HeaderCartButton = (props) => {
         <CartIcon></CartIcon>
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
